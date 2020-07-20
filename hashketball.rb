0de76ab4786d9adca9d0a1b3019fdfcea83a1022
Shelley Brook <sbrook13@gmail.com>
2020-07-20 15:02:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -157,7 +157,6 @@ end
 def shoe_size(name)
     size = 0
     game_hash.each do |location, team_hash|
-      team_hash.each do |key, value|
         team_hash[:players].each do |player|
           if player[:player_name] == name
             size = player[:shoe]
