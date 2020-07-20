c11a1dddc30c358ce14f70ec03ffb0df43918ce0
Shelley Brook <sbrook13@gmail.com>
2020-07-20 15:07:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -183,8 +183,8 @@ def team_colors(team_name)
   team_colors = []
   game_hash.each do |location, team_hash|
     team_hash[:team_name].each do |team|
-      if team[:team_name] == team_name 
-        team_colors = team[:colors]
+      if team_hash[:team_name] == team_name 
+        team_colors = team_hash[:colors]
       end
     end  
   end
